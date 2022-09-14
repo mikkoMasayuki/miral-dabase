a49cbab51edeae873d188f8ae903d39eb9cd8246
Mikko-Aasahi <mikko@createdbyblack.com>
2022-09-14 08:58:11 +0800

    cascade

--- migrations/20220725084942_create-project-images.js
+++ migrations/20220725084942_create-project-images.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('project_image', function (table) {
       table.increments()
       table.string('image_url')
-      table.integer('project_id').unsigned().references('project.id')
+      table.integer('project_id').unsigned().references('project.id').onDelete('CASCADE')
       table.timestamps(true, true)
     })
   };
